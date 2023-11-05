97aa5ea7afa9ab0bd9e11e374b9685db8efe7a00
Daniel Flügel <dflugel@student.42heilbronn.de>
2023-11-05 19:17:23 +0100

    Finished working on list.

---
+++ ft_atoi.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 04:55:51 by dflugel           #+#    #+#             */
+/*   Updated: 2023/11/05 18:29:51 by dflugel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ft_atoi(char *str)
+{
+    int nbr;
+    nbr = 0;
+
+    int i;
+    i = 0;
+
+    while (*(str + i) != '\0')
+    {
+        if (*(str + i) < 48 || *(str + i) > 57 )
+        {
+            return 0;
+        }
+        
+        nbr = nbr * 10 + (*(str + i) - 48);
+        i++;
+    }
+    
+    return(nbr);
+
+}
+
+
+int main () {
+   int val;
+   char str[20];
+   
+   strcpy(str, "98990489");
+   val = ft_atoi(str);
+   printf("String value = %s, Int value = %d\n", str, val);
+
+   strcpy(str, "tutorialspoint.com");
+   val = ft_atoi(str);
+   printf("String value = %s, Int value = %d\n", str, val);
+
+   return(0);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 04:55:57 by dflugel           #+#    #+#             */
+/*   Updated: 2023/11/05 19:16:53 by dflugel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+#include <stdio.h>
+#include <string.h>
+
+char *ft_strnstr(char *haystack, char *needle, size_t len)
+{
+    size_t i;
+    i = 0;
+    size_t j;
+    j = 0;
+
+    while (*(haystack + i) != '\0')
+    {
+        if (i == len)
+        {
+            return 0;
+        }
+        while (*(haystack + i + j) == *(needle + j))
+        {
+            j++;
+            if (*(needle + j) == '\0')
+            {
+                return (haystack + i);
+            }
+        }
+        j = 0;
+        i++;
+    }
+return 0;
+
+}
+
+
+int main()
+{
+    char test[] = "This is my test";
+    char try[] = "is";
+    
+    // testen
+    puts(test);
+
+    puts(ft_strnstr(test, try, 20));
+
+    return 0;
+}
