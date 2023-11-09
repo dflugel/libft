89d1caa9acc5f3529c094397137689a457700c32
Daniel Flügel <dflugel@student.42heilbronn.de>
2023-11-09 06:25:37 +0100

    worked on ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 05:54:10 by dflugel           #+#    #+#             */
+/*   Updated: 2023/11/09 06:24:52 by dflugel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *ft_substr(char const *s, unsigned int start, size_t len);
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char * substring;
+    size_t i;
+    i = 0;
+
+    substring = malloc(len);
+
+    while (*(s + start + i) != '\0')
+    {
+        if (i == len)
+        {
+            break;
+        }
+        *(substring + i) = *(s + start + i);
+        i++;
+    }
+
+    *(substring + i) = '\0';
+
+    return substring;
+    //return null if alloc fails not implemented yet
+}
+
+int main()
+{
+    char test[] = "This is my test";
+
+    
+    // testen
+    puts(test);
+
+
+    puts(ft_substr(test, 3, 7));
+
+
+    return 0;
+}
