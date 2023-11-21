e3b0b0410ec9f73930a35f9290a5f646c9ea2e9f
Daniel Flügel <dflugel@student.42heilbronn.de>
2023-11-21 18:02:07 +0100

    added start of split

---
+++ ft_split.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 18:01:37 by dflugel           #+#    #+#             */
+/*   Updated: 2023/11/21 18:01:47 by dflugel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    **ft_split(char const *s, char c)
+{
+    
+}
+
+int     count_delimeters(char const *s, char c)
+{
+    int     i;
+
+    i = 0;
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            i++;
+        }
+        
+        s++;
+    }
+    return (i);
+}
+
+int     main(void)
+{
+    char    test[] = "Dies is mein Test.";
+    char    try;
+
+    try = ' ';
+    printf("Es gibt %i Delimeter\n", count_delimeters(test, try));
+    return 0;
+}
